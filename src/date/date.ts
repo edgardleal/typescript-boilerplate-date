30e78b2cce27b4ebf7a3273d9ad16c2b69f036c1
edgardleal <edgardleal@gmail.com>
2021-11-15 17:23:58 -0300

    🐣 initial commit

---
+++ src/date/date.ts
@@ -0,0 +1,43 @@
+/**
+ * date.ts
+ * Copyright (C) 2021 edgardleal
+ *
+ * Distributed under terms of the MIT license.
+ */
+function twoDigits(value: number): string {
+  if (value < 10) {
+    return `0${value}`;
+  }
+  return `${value}`;
+}
+
+/**
+ * Represents a date with internal functionalities
+ */
+export default class InternalDate {
+  private nativeDate: Date;
+
+  constructor(date: Date = new Date()) {
+    this.nativeDate = date;
+  }
+
+  /**
+   * return a copy of native Date instance
+   *
+   * @returns {Date}
+   */
+  getDate(): Date {
+    return new Date(this.nativeDate);
+  }
+
+  toString(): string {
+    const year = this.nativeDate.getFullYear();
+    const month = twoDigits(this.nativeDate.getMonth());
+    const day = twoDigits(this.nativeDate.getDay());
+
+    const hour = twoDigits(this.nativeDate.getHours());
+    const minutes = twoDigits(this.nativeDate.getMinutes());
+    const seconds = twoDigits(this.nativeDate.getSeconds());
+    return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
+  }
+}
